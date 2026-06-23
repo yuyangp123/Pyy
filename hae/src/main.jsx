b395a2a7d3b788bc7a8c18613a0718f0ccbc949d
yuyangp123 <218974949+yuyangp123@users.noreply.github.com>
2026-06-23 16:08:16 +0100

    Move Health Dashboard to HAE repo

--- hae/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import IntegratedReport from "./IntegratedReport.jsx";
-
-createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <IntegratedReport />
-  </React.StrictMode>
-);
--- hae/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// Project pages are served from https://<user>.github.io/Pyy/
-export default defineConfig(({ command }) => ({
-  plugins: [react()],
-  base: command === "build" ? "/Pyy/" : "/",
-}));
